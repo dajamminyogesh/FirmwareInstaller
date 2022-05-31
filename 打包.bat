f6da0b85d036e8a593bca67147e12d4e682ee771
bianruifeng <912736557@qq.com>
2022-05-31 11:45:29 +0800

    打包前移除旧的编译打包文件

--- 打包.bat
+++ 打包.bat
@@ -1,2 +1,9 @@
-rem call "%USERPROFILE%\.virtualenvs\FirmwareInstaller\Scripts\activate.bat"
+@REM rem call "%USERPROFILE%\.virtualenvs\FirmwareInstaller\Scripts\activate.bat"
+
+DEL /F /A /Q Package\build
+RD /S /Q Package\build
+DEL /F /A /Q Package\dist
+RD /S /Q Package\dist
+
+call "C:\Users\User\Desktop\FirmwareInstaller\FirmwareInstaller\venv\Scripts\activate.bat"
 @pyinstaller --workpath Package/build --distpath Package/dist -y Installer.spec
